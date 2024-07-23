ce41591236ed383556a83d25df2e9cca5282857a
Md Ferdouse Hossain Bhuiya <ferdousbhuiya@gmail.com>
2024-07-23 14:09:57 -0400

    Hello Final Blog

--- Blog_Multiple_photos/main.py
+++ Blog_Multiple_photos/main.py
@@ -67,7 +67,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI",'sqlite:///posts.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -324,4 +324,4 @@ def contact():
     return render_template('contact.html', form=form, msg_sent=False)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False)
